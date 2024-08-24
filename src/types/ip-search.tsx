ea150a82cedd482131f3fec881aa1bb0f4505ab6
Dani47 <87781904+Danii47@users.noreply.github.com>
2024-08-25 01:29:47 +0200

    feat: add types

--- src/types/ip-search.tsx
+++ src/types/ip-search.tsx
@@ -1,4 +1,9 @@
 export interface ipSearch {
+  freeIpApiData: FreeIpApi
+  ipApiData: IpApi
+}
+
+interface FreeIpApi {
   cityName: string
   continent: string
   continentCode: string
@@ -18,7 +23,34 @@ export interface ipSearch {
   zipCode: string
 }
 
-export interface Currency {
+interface Currency {
   code: string
   name: string
+}
+
+interface IpApi {
+  status: string
+  continent: string
+  continentCode: string
+  country: string
+  countryCode: string
+  region: string
+  regionName: string
+  city: string
+  district: string
+  zip: string
+  lat: number
+  lon: number
+  timezone: string
+  offset: number
+  currency: string
+  isp: string
+  org: string
+  as: string
+  asname: string
+  reverse: string
+  mobile: boolean
+  proxy: boolean
+  hosting: boolean
+  query: string
 }
