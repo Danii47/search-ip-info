f2ba2f77f7e398f7a1ff1bb5345799431b66b553
Dani47 <87781904+Danii47@users.noreply.github.com>
2024-09-01 18:40:03 +0200

    feat: show map

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css'
 
 function App() {
   const [ipInfo, setIpInfo] = useState<ipSearch>()
-  const [userIP, setUserIP] = useState<string>()
+  const [userIP, setUserIP] = useState<string>("")
 
   const getUserIP = async () => {
     const response = await fetch('https://api.ipify.org?format=json')
@@ -38,40 +38,53 @@ function App() {
         <input id="ip" type="text" placeholder="1.1.1.1" value={userIP} onChange={handleIPChange} />
         <button type="submit">Buscar</button>
       </form>
+      
       {
         ipInfo && (
           <div className="ip-info-container">
-
             {
               ipInfo.ipApiData.status === "success" ? (
                 <>
-                  <h2>Información de la IP</h2>
-                  <p><strong>IP:</strong> {ipInfo.freeIpApiData.ipAddress}</p>
-                  <p><strong>Version de IP:</strong> {ipInfo.freeIpApiData.ipVersion}</p>
-                  <p><strong>Continente:</strong> {ipInfo.freeIpApiData.continent}</p>
-                  <p><strong>Código de continente:</strong> {ipInfo.freeIpApiData.continentCode}</p>
-                  <p><strong>País:</strong> {ipInfo.freeIpApiData.countryName}</p>
-                  <p><strong>Código de país:</strong> {ipInfo.freeIpApiData.countryCode}</p>
-                  <p><strong>Región:</strong> {ipInfo.freeIpApiData.regionName}</p>
-                  <p><strong>Ciudad:</strong> {ipInfo.freeIpApiData.cityName}</p>
-                  <p><strong>Distrito:</strong> {ipInfo.ipApiData.district || "No encontrado"}</p>
-                  <p><strong>Código postal:</strong> {ipInfo.ipApiData.zip}</p>
-                  <p><strong>Latitud:</strong> {ipInfo.ipApiData.lat}</p>
-                  <p><strong>Longitud:</strong> {ipInfo.ipApiData.lon}</p>
-                  <p><strong>Zona horaria:</strong> {ipInfo.freeIpApiData.timeZone}</p>
-                  {/* <p><strong>Idioma:</strong> {ipInfo.freeIpApiData.language}</p> */}
-                  <p><strong>Moneda:</strong> {ipInfo.freeIpApiData.currency.name} ({ipInfo.freeIpApiData.currency.code})</p>
-                  <p><strong>Proxy:</strong> {ipInfo.freeIpApiData.isProxy ? 'Sí' : 'No'}</p>
-                  <p><strong>Es móvil:</strong> {ipInfo.ipApiData.mobile ? 'Sí' : 'No'}</p>
-                  <p><strong>Hosting:</strong> {ipInfo.ipApiData.hosting ? 'Sí' : 'No'}</p>
-                  <p><strong>Zonas horarias:</strong> {ipInfo.freeIpApiData.timeZones.join(', ')}</p>
-                  <p><strong>ISP:</strong> {ipInfo.ipApiData.isp}</p>
-                  <p><strong>Organización:</strong> {ipInfo.ipApiData.org}</p>
-                  <p><strong>AS:</strong> {ipInfo.ipApiData.as}</p>
-                  <p><strong>Nombre AS:</strong> {ipInfo.ipApiData.asname}</p>
-                  <p><strong>Reverso:</strong> {ipInfo.ipApiData.reverse}</p>
-                  <p><strong>TLDs:</strong> {ipInfo.freeIpApiData.tlds.join(', ')}</p>
-                </>
+                  <div>
+                    <h2>Información de la IP</h2>
+                    <p><strong>IP:</strong> {ipInfo.freeIpApiData.ipAddress}</p>
+                    <p><strong>Version de IP:</strong> {ipInfo.freeIpApiData.ipVersion}</p>
+                    <p><strong>Continente:</strong> {ipInfo.freeIpApiData.continent}</p>
+                    <p><strong>Código de continente:</strong> {ipInfo.freeIpApiData.continentCode}</p>
+                    <p><strong>País:</strong> {ipInfo.freeIpApiData.countryName}</p>
+                    <p><strong>Código de país:</strong> {ipInfo.freeIpApiData.countryCode}</p>
+                    <p><strong>Región:</strong> {ipInfo.freeIpApiData.regionName}</p>
+                    <p><strong>Ciudad:</strong> {ipInfo.freeIpApiData.cityName}</p>
+                    <p><strong>Distrito:</strong> {ipInfo.ipApiData.district || "No encontrado"}</p>
+                    <p><strong>Código postal:</strong> {ipInfo.ipApiData.zip}</p>
+                    <p><strong>Latitud:</strong> {ipInfo.ipApiData.lat}</p>
+                    <p><strong>Longitud:</strong> {ipInfo.ipApiData.lon}</p>
+                    <p><strong>Zona horaria:</strong> {ipInfo.freeIpApiData.timeZone}</p>
+                    {/* <p><strong>Idioma:</strong> {ipInfo.freeIpApiData.language}</p> */}
+                    <p><strong>Moneda:</strong> {ipInfo.freeIpApiData.currency.name} ({ipInfo.freeIpApiData.currency.code})</p>
+                    <p><strong>Proxy:</strong> {ipInfo.freeIpApiData.isProxy ? 'Sí' : 'No'}</p>
+                    <p><strong>Es móvil:</strong> {ipInfo.ipApiData.mobile ? 'Sí' : 'No'}</p>
+                    <p><strong>Hosting:</strong> {ipInfo.ipApiData.hosting ? 'Sí' : 'No'}</p>
+                    <p><strong>Zonas horarias:</strong> {ipInfo.freeIpApiData.timeZones.join(', ')}</p>
+                    <p><strong>ISP:</strong> {ipInfo.ipApiData.isp}</p>
+                    <p><strong>Organización:</strong> {ipInfo.ipApiData.org}</p>
+                    <p><strong>AS:</strong> {ipInfo.ipApiData.as}</p>
+                    <p><strong>Nombre AS:</strong> {ipInfo.ipApiData.asname}</p>
+                    <p><strong>Reverso:</strong> {ipInfo.ipApiData.reverse}</p>
+                    <p><strong>TLDs:</strong> {ipInfo.freeIpApiData.tlds.join(', ')}</p>
+                    </div>
+                    <div>
+                      <iframe
+                        width="600"
+                        height="450"
+                        loading="lazy"
+                        allowFullScreen
+                        referrerPolicy="no-referrer-when-downgrade"
+                        src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBoFf2mJZpgwrxBNmm3ZxGPCGQKe1Tykxw
+                          &q=${ipInfo.ipApiData.lat},${ipInfo.ipApiData.lon}`}>
+                      </iframe>
+                    </div>
+                  </>
               ) : (
                 <strong>No se encontró información para la IP ingresada.</strong>
               )
